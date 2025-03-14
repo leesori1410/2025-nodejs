a2311a993323b97c036d35f699bd157fd1bbd6b6
leesori1410 <w2319@e-mirim.hs.kr>
2025-03-14 12:18:35 +0900

    for, for-of

---
+++ grammer/for.js
@@ -0,0 +1,15 @@
+const letter = ['a', 'b', 'c', 'd'];
+
+for(let i=0; i<letter.length; i++){
+    console.log(letter[i]);
+}
+
+letter.forEach(function(l){
+    console.log(l);
+})
+
+for(const l of letter){
+    console.log(l);
+}
+
+letter.forEach(l => console.log(l));
