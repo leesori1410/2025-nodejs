673dacd53067217a6edd1fefe6c69573ca0d5e25
leesori1410 <w2319@e-mirim.hs.kr>
2025-04-02 09:43:20 +0900

    .env

--- server2.js
+++ server2.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = 2007;
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'mirim4',
-  database: 'traveldb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
