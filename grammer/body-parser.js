93fcaac5f5b683083125e72a0dc3b07ed237c27b
leesori1410 <w2319@e-mirim.hs.kr>
2025-04-04 12:16:06 +0900

    body-parser 신버전

--- grammer/body-parser.js
+++ grammer/body-parser.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extends: true}));
+app.use(express.json());
+app.use(express.urlencoded({extends: true}));
 
 app.post('/submit', (req, res) => {
   const {name, year} = req.body;
