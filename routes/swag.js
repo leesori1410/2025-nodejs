f0355fbe6538e07bacedb25916c71142c651fbbe
leesori1410 <w2319@e-mirim.hs.kr>
2025-04-16 16:06:37 +0900

    Router

--- routes/swag.js
+++ routes/swag.js
@@ -1,4 +1,36 @@
 const express = require('express');
+const path = require('path');
+const mysql = require('mysql2');
+const methodOverride = require('method-override');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = 2007;
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect(err => {
+  if(err) {
+    console.error('MySQL 연결 실패: ', err);
+    return;
+  }
+  console.log('MySQL 열결 성공');
+})
+
+app.use(methodOverride('_method'));
+app.use(express.json());
+app.use(express.urlencoded({extends: true}));
+
+app.set('view engine', 'ejs');
+// __dirname : 현재 파일이 속해있는 디렉토리의 절대경로
+// path.join : 운영체제에 맞추어 경로지정자(\, /)를 설정해준다
+app.set('views', path.join(__dirname, 'views'));
 
 const router = express.Router();
 
--- server2.js
+++ server2.js
@@ -1,120 +1,21 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2');
 const methodOverride = require('method-override');
-const dotenv = require('dotenv');
-dotenv.config();
+const travelRouters = require('./routes/travel');
 
 const app = express();
-const port = 2007;
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-db.connect(err => {
-  if(err) {
-    console.error('MySQL 연결 실패: ', err);
-    return;
-  }
-  console.log('MySQL 열결 성공');
-})
+const port = 2010;
 
 app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({extends: true}));
-
+              
 app.set('view engine', 'ejs');
 // __dirname : 현재 파일이 속해있는 디렉토리의 절대경로
 // path.join : 운영체제에 맞추어 경로지정자(\, /)를 설정해준다
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/travel', (req, res) => {
-  const _query = 'SELECT * FROM travellist';
-  db.query(_query, (err, results) => {
-    if(err){
-      console.error('데이터베이스 쿼리 실패', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    const travelList = results;
-    res.render('travel', {travelList});
-  });
-});
-
-app.get('/travel/:id', (req, res) => {
-  const travelId = req.params.id;
-  const _query = 'SELECT * FROM travellist WHERE id = ?';
-  db.query(_query, [travelId],(err, results) => {
-    if(err){
-      console.error('데이터베이스 쿼리 실패', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    const travel = results[0];
-    res.render('travelDetail', {travel});
-  });
-});
-
-app.post('/travel', (req, res) => {
-  const {name} = req.body;
-  const _query = 'INSERT INTO travellist (name) VALUES (?)';
-  db.query(_query, [name],(err, results) => {
-    if(err){
-      console.error('데이터베이스 쿼리 실패', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    res.redirect('/travel');
-  });
-})
-
-app.put('/travel/:id', (req, res) => {
-  const travelId = req.params.id;
-  const {name} = req.body;
-  const _query = 'UPDATE travelList SET name = ? WHERE id  = ?';
-  db.query(_query, [name, travelId],(err, results) => {
-    if(err){
-      console.error('데이터베이스 쿼리 실패', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    res.render('updateSuccess');
-  });
-});
-
-app.get('/travel/:id/edit', (req, res) => {
-  const travelId = req.params.id;
-  const _query = 'SELECT * FROM travellist WHERE id = ?';
-  db.query(_query, [travelId],(err, results) => {
-    if(err){
-      console.error('데이터베이스 쿼리 실패', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    const travel = results[0];
-    res.render('editTravel', {travel});
-  });
-});
-
-app.get('/add-travel', (req, res) => [
-  res.render('addTravel')
-]);
-
-app.delete('/travel/:id', (req, res) => {
-  const travelId = req.params.id;
-  const _query = 'DELETE FROM travelList WHERE id = ?';
-  db.query(_query, [travelId], (err, results) => {
-    if(err) {
-      console.err('DB 쿼리 실패', err);
-      res.status(500).send('Internal Server Error');
-    }
-    res.render('deleteSuccess');
-  });
-});
+app.use('/travel', travelRouters);
 
 app.listen(port, () => {
   console.log(`Express server running at http://localhost:${port}`);
